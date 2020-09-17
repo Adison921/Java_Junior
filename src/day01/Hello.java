00257e7bf0aea0576cb72fe5e9af3daaad444386
Adison <2805422539@qq.com>
2020-09-17 15:39:17 +0800

    1

--- src/day01/Hello.java
+++ src/day01/Hello.java
@@ -1,8 +1,12 @@
 package day01;
 
+/*
+HelloWorld程序测试
+* */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
     }
 }
+
+
---
+++ src/day01/Identifier.java
@@ -0,0 +1,12 @@
+package day01;
+
+/**
+ * @author zsh
+ * @create 2020-09-17-15:30
+ */
+public class Identifier {
+    public static void main(String[] args) {
+
+    }
+
+}
